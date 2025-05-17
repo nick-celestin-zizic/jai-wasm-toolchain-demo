dd8bf661d0b5377e7a29d5ada394abb09deb4af4
nick-celestin-zizic <51030121+nick-celestin-zizic@users.noreply.github.com>
2025-05-16 20:33:08 -0400

    Service Worker Test 2

--- examples/Input/worker.js
+++ examples/Input/worker.js
@@ -1,43 +1,36 @@
-const CACHE_NAME = "Cache - Input";
+const CACHE_NAME = "Cache - Input - ";
+const CACHE_TIME = "2025-05-17T00:29:23.012Z";
 const PATHS_TO_CACHE = [ "", "index.html", "icon.png" , "main.wasm", "manifest.json", "runtime.js", ];
 
-self.addEventListener("fetch", (event) => {
-    event.respondWith((async () => {
-        const response = await caches.match(event.request.url);
-        // console.log("GOT RESPONSE", response);
-        
-        if (response) {
-        //     console.log("returning cached response");
-            return response;
+const maybe_refresh_caches = async () => {
+    const keys = await caches.keys();
+    let found  = false;
+    for (const cache_name of keys) {
+        if (!cache_name.startsWith(CACHE_NAME)) continue;
+        if (!cache_name.endsWith(CACHE_TIME)) {
+            // console.log(CACHE_NAME, "deleting outdated cache ", cache_name);
+            await caches.delete(cache_name);
         } else {
-        //     console.log("did not find cached response");
-            return fetch(event.request);
+            // console.log(CACHE_NAME, "found valid cache ", cache_name);
+            found = true;
         }
-    })());
-});
-
-self.addEventListener("activate", (event) => {
-    // console.log("ACTIVATE");
-});
-
-self.addEventListener("install", (event) => {
-    // console.log("INSTALL");
-    // const white_list = [CACHE_NAME];
+    }
     
-    self.skipWaiting();
-    event.waitUntil((async () => {
-        // clean up old cache
-        console.log("INSTALL");
-        const cache_names = (await caches.keys()).filter((name) => name === CACHE_NAME);
-        await Promise.all(cache_names.map((name) => { return caches.delete(name); }));
-        
-        // cache the files we need for offline use
-        const cache = await caches.open(CACHE_NAME);
+    if (!found) {
+        // console.log(CACHE_NAME+CACHE_TIME, "creating cache for the first time");
+        const cache   = await caches.open(CACHE_NAME + CACHE_TIME);
         const clients = await self.clients.matchAll({includeUncontrolled: true, type: "window"});
         if (clients.length < 1) throw new Error(`TODO ${clients.length}`);
         
         // the spec states that the first element of this array should
         // be the window that started the worker
-        await cache.addAll(PATHS_TO_CACHE.map((name) => clients[0].url + name));
-    })());
+        const client  = clients[0];
+        await cache.addAll(PATHS_TO_CACHE.map((name) => client.url + name));
+    }
+};
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith((async () => (await caches.match(event.request.url)) || fetch(event.request))());
 });
+self.addEventListener("install", (event) => { event.waitUntil(maybe_refresh_caches()); });
+self.addEventListener("activate", (event) => { event.waitUntil(maybe_refresh_caches()); });
